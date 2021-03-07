30ce6bf3e512fbed952607f3000ec09185821bd3
Juan <jpalenzuela@outlook.com>
2021-03-07 00:08:43 +0000

    added search by id

--- backend/PayMe/src/main/java/com/codeclan/example/PayMe/controllers/UserController.java
+++ backend/PayMe/src/main/java/com/codeclan/example/PayMe/controllers/UserController.java
@@ -4,9 +4,11 @@ import com.codeclan.example.PayMe.models.User;
 import com.codeclan.example.PayMe.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -17,6 +19,11 @@ public class UserController {
     @GetMapping(value = "/users")
     public List<User> getAllUsers(){
         return userRepository.findAll();
+    };
+
+    @GetMapping(value = "/users/{id}")
+    public Optional<User> getUser(@PathVariable Long id) {
+        return userRepository.findById(id);
     };
 
 }
